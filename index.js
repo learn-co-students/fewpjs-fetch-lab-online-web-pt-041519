6eb074708eb1b257347acac9176535e8a94507a8
CindyChenx <chen_cx0128@hotmail.com>
2019-11-08 07:42:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-function fetchBooks() {
+const gameURL = "https://anapioficeandfire.com/api/books";
 
+function fetchBooks() {
+  fetch(gameURL)
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+  });
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+  fetchBooks();
 })
