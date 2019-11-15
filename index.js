572b4893c7ff6ab5403de13081abecf402977b68
github username <github email address>
2019-11-15 07:25:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 function fetchBooks() {
-  return fetch('https://anapioficeandfire.com/api/books')
-    .then(resp => resp.json())
-    .then(json => renderBooks(json))
+   return fetch('https://anapioficeandfire.com/api/books')
+      .then(resp => resp.json())
+      .then(json => renderBooks(json))
 }
+
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+   // want to add to the main
+   const main = document.querySelector('main')
+
+   // want to loop thru the json, create a new h1, get the title name, set that as the h1 inner text, and append to the main
+   json.forEach(book => {
+      const h1 = document.createElement('h1')
+      h1.innerHTML = `<h1>${book.name}</h1>`
+      main.appendChild(h1)
+   })
+   
+
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+   fetchBooks()
+ })
